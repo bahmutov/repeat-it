ecff924aafd56f1ac9e84324706de17461df4baa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-08 23:53:27 -0400

    initial

---
+++ src/repeat-it-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const repeatIt = require('.')
+
+describe('repeat-it', () => {
+  it('write this test', () => {
+    console.assert(repeatIt, 'should export something')
+  })
+})
